9a3afa66db5759783bcca804e8db6f6fd2be4003
Philip Lombardi <plombardi@datawire.io>
2016-09-12 11:30:16 -0400

    Make the MDK a dependency of the project!

--- {{ cookiecutter.repo_name }}/src/main/java/{{ cookiecutter.project_package_path }}/ServiceVerticle.java
+++ {{ cookiecutter.repo_name }}/src/main/java/{{ cookiecutter.project_package_path }}/ServiceVerticle.java
@@ -6,14 +6,24 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 
 import java.time.Instant;
 
 public class ServiceVerticle extends AbstractVerticle {
 
+  private final static Logger logger = LoggerFactory.getLogger(ServiceVerticle.class);
+
   public ServiceVerticle() {
     super();
+  }
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    logger.info("Datawire MDK started");
+    super.start(startFuture);
   }
 
   public void start() throws Exception {
